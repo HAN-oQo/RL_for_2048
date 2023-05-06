1f5630b1beac2568b9908dd99852be74df7e926a
HAN-oQo <hanq0212@gmail.com>
2023-05-07 00:07:00 +0900

    fix docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -ti --gpus '"device='$1'"' -v /danube/hankyu/.desktop/project/RL_for_2048:/app --ipc=host --name $2 voithru /bin/bash
+docker run -ti --gpus '"device='$1'"' -v /project/RL_for_2048:/app --ipc=host --name $2 2048 /bin/bash
 # ex) docker run -ti --gpus '"device='$1'"' -v <put_your_working_directory>:/app --ipc=host --name $2 <put_your_docker_image_name> /bin/bash
